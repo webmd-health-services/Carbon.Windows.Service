d0ea9a78f68b05f98cbc60ff4770a41acedf4e09
Aaron Jensen <ajensen@webmd.net>
2026-07-21 16:36:19 -0700

    Migrated Test-CService tests to Pester 5.

--- Tests/Test-CService.Tests.ps1
+++ Tests/Test-CService.Tests.ps1
@@ -1,47 +1,42 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-#Requires -Version 1
+#Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-CarbonTest.ps1' -Resolve)
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
 
-Describe 'Test-Service when testing an existing service' {
-    $error.Clear()
-    Get-Service | ForEach-Object {
-        It ('should find the {0} {1} service' -f $_.ServiceName,$_.ServiceType) {
-            Test-Service -Name $_.ServiceName | Should Be $true
-            $Error.Count | Should Be 0
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.Windows.Service' -Resolve) -Verbose:$false
+}
+
+Describe 'Test-CService' {
+    BeforeEach {
+        $Global:Error.Clear()
+    }
+
+    Context 'service exists' {
+        $svcNames =
+            Get-Service -ErrorAction Ignore |
+            # GoogleUpdaterInternalService causes intermittent problems on the build servers
+            Where-Object 'ServiceName' -NotLike 'GoogleUpdater*' |
+            Select-Object -ExpandProperty 'ServiceName'
+        It 'returns true for <_> service' -ForEach $svcNames {
+            Test-CService -Name $_ | Should -BeTrue
+            $Global:Error | Should -BeNullOrEmpty
         }
     }
-}
 
-Describe 'Test-Service when testing for a non-existent service' {
-    
-    $error.Clear()
-
-    It 'should not find missing service' {
-        (Test-Service -Name 'ISureHopeIDoNotExist') | Should Be $false
-        $error.Count | Should Be 0
+    Context 'service does not exist' {
+        It 'returns false' {
+            Test-CService -Name 'ISureHopeIDoNotExist' | Should -BeFalse
+        }
     }
-    
-}
 
-Describe 'Test-Service when testing for a device service' {
-    $Error.Clear()
-    [ServiceProcess.ServiceController]::GetDevices() | ForEach-Object {
-        It ('should find the {0} {1} service' -f $_.ServiceName,$_.ServiceType) {
-            Test-Service -Name $_.ServiceName | Should Be $true
-            $Error.Count | Should Be 0
+    Context 'service is device driver' {
+        $svcNames = [ServiceProcess.ServiceController]::GetDevices() | Select-Object -ExpandProperty 'ServiceName'
+
+        It 'returns true for <_> driver' -ForEach $svcNames {
+            Test-CService -Name $_ | Should -BeTrue
+            $Global:Error | Should -BeNullOrEmpty
         }
     }
 }
